fab5237d1de738e9435d54890c61b1417779438a
Sajith <sajithdil@gmail.com>
2013-07-05 13:33:01 +0530

    built the shell

---
+++ FantasyElementsRPGClient/FantasyElementsRPG.Shell/FantasyElementsRPG_Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Practices.Unity;
+
+namespace FantasyElementsRPG.Shell
+{
+    public class FantasyElementsRPG_Bootstrapper : UnityBootstrapper
+    {
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<FantasyElementsRPG_Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.RootVisual = (UIElement)this.Shell;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ An RPG Game
 Fantasy: vampires werewolves and stuff like that
 Elements: The four Elements: Air, Earth, Water, Fire
 RPG: you know what that is :D
+
+Project is built on: Composite Application Library (CAL)
