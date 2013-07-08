b81935dfe91cba9e3805dd0169eff6812fd0164a
Sajith <sajithdil@gmail.com>
2013-07-08 11:38:23 +0530

    built policy server

---
+++ FantasyElementsRPG/FantasyElementsRPG.Server.PolicyServer.Logger/PolicyServerLogger.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace FantasyElementsRPG.Server.PolicyServer.Logger
+{
+    public sealed class PolicyServerLogger
+    {
+        #region singleton region
+        //private instance
+        private static volatile PolicyServerLogger iLog;
+        private static object syncRoot = new Object();
+        //private instance
+        private PolicyServerLogger()
+        {
+            uLog.CreateFile();
+        }
+
+        //public getter
+        public static PolicyServerLogger PolicyServerLog
+        {
+            get
+            {
+                if (iLog == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (iLog == null)
+                            iLog = new PolicyServerLogger();
+                    }
+                }
+
+                return iLog;
+            }
+        }
+        #endregion
+
+        #region processes
+
+        #region variables
+        private RichTextBox itextbox = new RichTextBox();
+        private UtilLog uLog = new UtilLog();
+        #endregion
+
+        public void SetTextBox(RichTextBox itextbox)
+        {
+            this.itextbox = itextbox;
+        }
+
+        /// <summary>
+        /// create file method to access the uLog util
+        /// </summary>
+        /// <returns></returns>
+        public bool CreateFile()
+        {
+            return uLog.CreateFile();
+        }
+
+        /// <summary>
+        /// write to log method to acces the util class, this sends in the Name as well
+        /// </summary>
+        /// <param name="formName"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        public bool WriteLog(string Name, string log)
+        {
+            
+            FlowDocument mcFlowDoc = new FlowDocument();
+            mcFlowDoc = itextbox.Document;
+            Paragraph pr = new Paragraph();
+            pr.Inlines.Add(Environment.NewLine + DateTime.Now + ": " +log);
+            mcFlowDoc.Blocks.Add(pr);
+            itextbox.Document = mcFlowDoc;
+
+            return uLog.WriteLog(Name, log);
+        }
+
+        /// <summary>
+        /// Creates an error log file
+        /// </summary>
+        /// <returns></returns>
+        public bool CreateErrorLog()
+        {
+            return uLog.CreateErrorLog();
+        }
+
+        /// <summary>
+        /// Writes to the error log file
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        public bool WriteErrorLog(string Name, string log)
+        {
+            FlowDocument mcFlowDoc = new FlowDocument();
+            mcFlowDoc = itextbox.Document;
+            Paragraph pr = new Paragraph();
+            pr.Inlines.Add(Environment.NewLine + DateTime.Now + " Error: " +log);
+            mcFlowDoc.Blocks.Add(pr);
+            itextbox.Document = mcFlowDoc;
+
+            return uLog.WriteErrorLog(Name, log);
+        }
+
+        #endregion
+    }
+}
---
+++ FantasyElementsRPG/FantasyElementsRPG.Server.PolicyServer.Logger/UtilLog.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyElementsRPG.Server.PolicyServer.Logger
+{
+    class UtilLog
+    {
+        //file location
+        string fileLocation = "Logs/";
+        string errLocation = "ErrorLogs/";
+        string fileName = "";
+        string errorFileName = "";
+        /// <summary>
+        /// Log file creator BL = if file exists delete it(to clear the log) then remake the file (empty log)
+        /// </summary>
+        /// <returns></returns>
+        public bool CreateFile()
+        {
+            FileStream fs = null;
+
+
+
+            fileName = fileLocation + "log-" + DateTime.Now.ToString("dd-mm-yyyy hh-mm-ss") + ".txt";
+            using (fs = File.Create(fileName))
+            {
+
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// The function that actually writes to the file, writes the form name as well
+        /// </summary>
+        /// <param name="formName"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public bool WriteLog(string formName, string log)
+        {
+
+            File.AppendAllText(fileName, Environment.NewLine + DateTime.Now + ": Form Name:" + formName + ", Log: " + log);
+            return true;
+        }
+
+        /// <summary>
+        /// Create an error log in the ErrorLog Folder
+        /// </summary>
+        /// <returns></returns>
+        public bool CreateErrorLog()
+        {
+            FileStream fs = null;
+            errorFileName = errLocation + "errorlog-" + DateTime.Now.ToString("dd-mm-yyyy hh-mm-ss") + ".txt";
+            using (fs = File.Create(errorFileName))
+            {
+
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Write to the error log file
+        /// </summary>
+        /// <param name="formName"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        public bool WriteErrorLog(string formName, string log)
+        {
+            File.AppendAllText(errorFileName, Environment.NewLine + DateTime.Now + ": Form Name:" + formName + ", Log: " + log);
+            return true;
+        }
+    }
+}
