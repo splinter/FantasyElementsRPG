9cd338632c4b89b52802c0d1edafdc20c6deaee7
Sajith <sajithdil@gmail.com>
2013-07-08 20:37:05 +0530

    server side exception factory created

---
+++ FantasyElementsRPG/FantasyElementsRPG.Server.GameServer.ExceptionFactory/ExceptionFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyElementsRPG.Server.GameServer.ExceptionFactory
+{
+    public static class ExceptionFactory
+    {
+        public static void HandleException(Exception e)
+        {
+
+        }
+
+        
+
+        public static void HandleFatalException(Exception e)
+        {
+            //Close the server application
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
